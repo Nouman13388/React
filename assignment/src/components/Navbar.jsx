06315ba0c6c00a658953db742f7f401a203f3788
Nouman13388 <SP22-BSE-103@CUILAHORE.EDU.PK>
2023-12-29 13:29:59 +0500

    redux done

--- assignment/src/components/Navbar.jsx
+++ assignment/src/components/Navbar.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
+   <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid d-flex justify-content-between align-items-center">
-        <h1 className="navbar-brand">Math Magicians</h1>
+        <h1 className="navbar-brand"><Link to="/" style={{ textDecoration: 'none' }}>Math Magicians</Link></h1>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
@@ -22,6 +23,7 @@ function Navbar() {
         </div>
       </div>
     </nav>
+   </>
   );
 }
 
